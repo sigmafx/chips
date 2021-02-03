7066091af1b6c3ba23b0a0151e9cabb092336418
ukdrobi <david_robinson@waters.com>
2021-02-03 18:14:25 +0000

    Tests

--- test/m6532_test.c
+++ test/m6532_test.c
@@ -2,7 +2,7 @@
 #define CHIPS_IMPL
 #include "m6532.h"
 
-void test_AllMemoryCanBeZero()
+void test_AllMemoryCanBeZero(void)
 {
     m6532_t riot;
     uint64_t pins = 0ULL;
@@ -31,7 +31,7 @@ void test_AllMemoryCanBeZero()
     }
 }
 
-void test_AllMemoryCanBeSet()
+void test_AllMemoryCanBeSet(void)
 {
     m6532_t riot;
     uint64_t pins = 0ULL;
@@ -63,7 +63,7 @@ void test_AllMemoryCanBeSet()
     }
 }
 
-void test_NoMemoryReadWhenCS1Reset()
+void test_NoMemoryReadWhenCS1Reset(void)
 {
     m6532_t riot;
     uint64_t pins = 0ULL;
@@ -92,7 +92,7 @@ void test_NoMemoryReadWhenCS1Reset()
     }
 }
 
-void test_NoMemoryReadWhenCS2Set()
+void test_NoMemoryReadWhenCS2Set(void)
 {
     m6532_t riot;
     uint64_t pins = 0ULL;
@@ -121,7 +121,7 @@ void test_NoMemoryReadWhenCS2Set()
     }
 }
 
-void test_NoMemoryReadWhenCS1ResetCS2Set()
+void test_NoMemoryReadWhenCS1ResetCS2Set(void)
 {
     m6532_t riot;
     uint64_t pins = 0ULL;
@@ -151,7 +151,7 @@ void test_NoMemoryReadWhenCS1ResetCS2Set()
     }
 }
 
-void test_PortA_DDR_ReadWrite()
+void test_PortA_DDR_ReadWrite(void)
 {
     m6532_t riot;
     uint64_t pins = 0ULL;
@@ -178,12 +178,10 @@ void test_PortA_DDR_ReadWrite()
 
         data = M6532_GET_DATA(pins);
         TEST_CHECK(data == ddr);
-
-        ddr++;
-    } while (ddr != 0x00);
+    } while (++ddr != 0x00);
 }
 
-void test_PortB_DDR_ReadWrite()
+void test_PortB_DDR_ReadWrite(void)
 {
     m6532_t riot;
     uint64_t pins = 0ULL;
@@ -210,12 +208,10 @@ void test_PortB_DDR_ReadWrite()
 
         data = M6532_GET_DATA(pins);
         TEST_CHECK(data == ddr);
-
-        ddr++;
-    } while (ddr != 0x00);
+    } while (++ddr != 0x00);
 }
 
-void test_PortA_DR_ReadWrite()
+void test_PortA_DR_ReadWrite(void)
 {
     m6532_t riot;
     uint64_t pins = 0ULL;
@@ -248,12 +244,10 @@ void test_PortA_DR_ReadWrite()
 
         data = M6532_GET_DATA(pins);
         TEST_CHECK(data == dr);
-
-        dr++;
-    } while (dr != 0x00);
+    } while (++dr != 0x00);
 }
 
-void test_PortB_DR_ReadWrite()
+void test_PortB_DR_ReadWrite(void)
 {
     m6532_t riot;
     uint64_t pins = 0ULL;
@@ -286,9 +280,213 @@ void test_PortB_DR_ReadWrite()
 
         data = M6532_GET_DATA(pins);
         TEST_CHECK(data == dr);
+    } while (++dr != 0x00);
+}
 
-        dr++;
-    } while (dr != 0x00);
+void test_PortA_Output(void)
+{
+    m6532_t riot;
+    uint64_t pins = 0ULL;
+    uint8_t data;
+
+    // Prepare
+    m6532_init(&riot);
+    M6532_SET_CS1(pins);
+    M6532_RESET_CS2(pins);
+    M6532_SET_RS(pins);
+
+    // Port A DDR all output
+    M6532_RESET_RW(pins);
+    M6532_SET_ADDR(pins, 0b0000001);
+    M6532_SET_DATA(pins, 0xFF);
+    pins = m6532_tick(&riot, pins);
+
+    // Port A DR
+    M6532_SET_ADDR(pins, 0b0000000);
+    uint8_t dr = 0x00;
+    do
+    {
+        M6532_RESET_RW(pins);
+        M6532_SET_DATA(pins, dr);
+        pins = m6532_tick(&riot, pins);
+
+        data = M6532_GET_PA(pins);
+        TEST_CHECK(data == dr);
+    } while (++dr != 0x00);
+}
+
+void test_PortB_Output(void)
+{
+    m6532_t riot;
+    uint64_t pins = 0ULL;
+    uint8_t data;
+
+    // Prepare
+    m6532_init(&riot);
+    M6532_SET_CS1(pins);
+    M6532_RESET_CS2(pins);
+    M6532_SET_RS(pins);
+
+    // Port B DDR all output
+    M6532_RESET_RW(pins);
+    M6532_SET_ADDR(pins, 0b0000011);
+    M6532_SET_DATA(pins, 0xFF);
+    pins = m6532_tick(&riot, pins);
+
+    // Port B DR
+    M6532_SET_ADDR(pins, 0b0000010);
+    uint8_t dr = 0x00;
+    do
+    {
+        M6532_RESET_RW(pins);
+        M6532_SET_DATA(pins, dr);
+        pins = m6532_tick(&riot, pins);
+
+        data = M6532_GET_PB(pins);
+        TEST_CHECK(data == dr);
+    } while (++dr != 0x00);
+}
+
+void test_PortA_Input(void)
+{
+    m6532_t riot;
+    uint64_t pins = 0ULL;
+    uint8_t data;
+
+    // Prepare
+    m6532_init(&riot);
+    M6532_SET_CS1(pins);
+    M6532_RESET_CS2(pins);
+    M6532_SET_RS(pins);
+
+    // Port A DDR all input
+    M6532_RESET_RW(pins);
+    M6532_SET_ADDR(pins, 0b0000001);
+    M6532_SET_DATA(pins, 0x00);
+    pins = m6532_tick(&riot, pins);
+
+    // Port A DR
+    M6532_SET_ADDR(pins, 0b0000000);
+    M6532_SET_RW(pins);
+    uint8_t input = 0x00;
+    do
+    {
+        M6532_SET_PA(pins, input);
+
+        pins = m6532_tick(&riot, pins);
+
+        data = M6532_GET_DATA(pins);
+        TEST_CHECK(data == input);
+        TEST_CHECK(M6532_GET_PA(pins) == input);
+    } while (++input != 0x00);
+}
+
+void test_PortB_Input(void)
+{
+    m6532_t riot;
+    uint64_t pins = 0ULL;
+    uint8_t data;
+
+    // Prepare
+    m6532_init(&riot);
+    M6532_SET_CS1(pins);
+    M6532_RESET_CS2(pins);
+    M6532_SET_RS(pins);
+
+    // Port B DDR all input
+    M6532_RESET_RW(pins);
+    M6532_SET_ADDR(pins, 0b0000011);
+    M6532_SET_DATA(pins, 0x00);
+    pins = m6532_tick(&riot, pins);
+
+    // Port B DR
+    M6532_SET_ADDR(pins, 0b0000010);
+    M6532_SET_RW(pins);
+    uint8_t input = 0x00;
+    do
+    {
+        M6532_SET_PB(pins, input);
+
+        pins = m6532_tick(&riot, pins);
+
+        data = M6532_GET_DATA(pins);
+        TEST_CHECK(data == input);
+        TEST_CHECK(M6532_GET_PB(pins) == input);
+    } while (++input != 0x00);
+}
+
+void test_PortA_InputOutput(void)
+{
+    m6532_t riot;
+    uint64_t pins = 0ULL;
+    uint8_t data;
+
+    // Prepare
+    m6532_init(&riot);
+    M6532_SET_CS1(pins);
+    M6532_RESET_CS2(pins);
+    M6532_SET_RS(pins);
+    M6532_RESET_RW(pins);
+
+    // Port A DDR - hi nibble input / lo nibble output
+    M6532_SET_ADDR(pins, 0b0000001);
+    M6532_SET_DATA(pins, 0x0F);
+    pins = m6532_tick(&riot, pins);
+
+    // Port A DR - All output bits set
+    M6532_SET_ADDR(pins, 0b0000000);
+    M6532_SET_DATA(pins, 0xFF);
+    pins = m6532_tick(&riot, pins);
+
+    M6532_SET_RW(pins);
+    uint8_t input = 0x00;
+    do
+    {
+        M6532_SET_PA(pins, input);
+
+        pins = m6532_tick(&riot, pins);
+
+        data = M6532_GET_DATA(pins);
+        TEST_CHECK(data == ((input & 0xF0) | 0x0F));
+        TEST_CHECK(M6532_GET_PA(pins) == ((input & 0xF0) | 0x0F));
+    } while (++input != 0x00);
+}
+
+void test_PortB_InputOutput(void)
+{
+    m6532_t riot;
+    uint64_t pins = 0ULL;
+    uint8_t data;
+
+    // Prepare
+    m6532_init(&riot);
+    M6532_SET_CS1(pins);
+    M6532_RESET_CS2(pins);
+    M6532_SET_RS(pins);
+    M6532_RESET_RW(pins);
+
+    // Port A DDR - hi nibble input / lo nibble output
+    M6532_SET_ADDR(pins, 0b0000011);
+    M6532_SET_DATA(pins, 0x0F);
+    pins = m6532_tick(&riot, pins);
+
+    // Port A DR - All output bits set
+    M6532_SET_ADDR(pins, 0b0000010);
+    M6532_SET_DATA(pins, 0xFF);
+    pins = m6532_tick(&riot, pins);
+
+    M6532_SET_RW(pins);
+    uint8_t input = 0x00;
+    do
+    {
+        M6532_SET_PB(pins, input);
+
+        pins = m6532_tick(&riot, pins);
+
+        data = M6532_GET_DATA(pins);
+        TEST_CHECK(data == ((input & 0xF0) | 0x0F));
+        TEST_CHECK(M6532_GET_PB(pins) == ((input & 0xF0) | 0x0F));
+    } while (++input != 0x00);
 }
 
 TEST_LIST = {
@@ -300,6 +498,12 @@ TEST_LIST = {
     { "test_PortA_DDR_ReadWrite", test_PortA_DDR_ReadWrite },
     { "test_PortB_DDR_ReadWrite", test_PortB_DDR_ReadWrite},
     { "test_PortA_DR_ReadWrite", test_PortA_DR_ReadWrite },
-    { "test_PortB_DR_ReadWrite", test_PortB_DR_ReadWrite},
+    { "test_PortB_DR_ReadWrite", test_PortB_DR_ReadWrite },
+    { "test_PortA_Output", test_PortA_Output },
+    { "test_PortB_Output", test_PortB_Output},
+    { "test_PortA_Input", test_PortA_Input},
+    { "test_PortB_Input", test_PortB_Input},
+    { "test_PortA_InputOutput", test_PortA_InputOutput},
+    { "test_PortB_InputOutput", test_PortB_InputOutput},
     { NULL, NULL }
 };
