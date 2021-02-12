80d9ba3cf81b8843be8bf2848d68dc680593fb09
sigmafx <david@sigmafx.co.uk>
2021-02-12 09:44:59 +0000

    Further updates

--- chips/tia.h
+++ chips/tia.h
@@ -63,12 +63,12 @@ extern "C" {
 #endif
 
 /* address bus pins */
-#define TIA_A0       (1ULL<<0)
-#define TIA_A1       (1ULL<<1)
-#define TIA_A2       (1ULL<<2)
-#define TIA_A3       (1ULL<<3)
-#define TIA_A4       (1ULL<<4)
-#define TIA_A5       (1ULL<<5)
+#define TIA_A0        (1ULL<<0)
+#define TIA_A1        (1ULL<<1)
+#define TIA_A2        (1ULL<<2)
+#define TIA_A3        (1ULL<<3)
+#define TIA_A4        (1ULL<<4)
+#define TIA_A5        (1ULL<<5)
 #define TIA_AB_PINS   (TIA_A0|TIA_A1|TIA_A2|TIA_A3|TIA_A4|TIA_A5)
 
 /* data bus pins */
@@ -88,15 +88,21 @@ extern "C" {
 #define TIA_CS2       (1ULL<<26)
 #define TIA_CS3       (1ULL<<27)
 #define TIA_RW        (1ULL<<28)
+#define TIA_RDY       (1ULL<<29)
 
 /* input port pins */
-#define TIA_I0       (1ULL<<48)
-#define TIA_I1       (1ULL<<49)
-#define TIA_I2       (1ULL<<50)
-#define TIA_I3       (1ULL<<51)
-#define TIA_I4       (1ULL<<52)
-#define TIA_I5       (1ULL<<53)
+#define TIA_I0        (1ULL<<48)
+#define TIA_I1        (1ULL<<49)
+#define TIA_I2        (1ULL<<50)
+#define TIA_I3        (1ULL<<51)
+#define TIA_I4        (1ULL<<52)
+#define TIA_I5        (1ULL<<53)
 #define TIA_PA_PINS   (TIA_I0|TIA_I1|TIA_I2|TIA_I3|TIA_I4|TIA_I5)
+
+enum TV {
+    NTSC = 0,
+    PAL = 1
+};
 
 /* I/O port state */
 typedef struct {
@@ -106,6 +112,11 @@ typedef struct {
 
 /* tia state */
 typedef struct {
+    enum TV tv;
+    uint16_t scanline;
+    uint8_t regWrite[0x2D];
+    uint8_t regRead[0x0D];
+    bool rdy;
 } tia_t;
 
 /* extract 8-bit data bus from 64-bit pins */
@@ -139,9 +150,11 @@ typedef struct {
 #define TIA_RESET_CS3(p) (p&=~TIA_CS3)
 #define TIA_SET_RW(p) (p|=TIA_RW)
 #define TIA_RESET_RW(p) (p&=~TIA_RW)
+#define TIA_SET_RDY(p) (p|=TIA_RDY)
+#define TIA_RESET_RDY(p) (p&=~TIA_RDY)
 
 /* initialize a new 6532 instance */
-void tia_init(tia_t* tia);
+void tia_init(tia_t* tia, enum TV tv);
 /* tick the tia */
 uint64_t tia_tick(tia_t* tia, uint64_t pins);
 
@@ -150,6 +163,7 @@ uint64_t tia_tick(tia_t* tia, uint64_t pins);
 #endif
 
 /*-- IMPLEMENTATION ----------------------------------------------------------*/
+#define CHIPS_IMPL 
 #ifdef CHIPS_IMPL
 #include <string.h>
 #ifndef CHIPS_ASSERT
@@ -157,11 +171,134 @@ uint64_t tia_tick(tia_t* tia, uint64_t pins);
 #define CHIPS_ASSERT(c) assert(c)
 #endif
 
-uint64_t tia_tick(tia_t* c, uint64_t pins) {
+// Write registers
+#define VSYNC   0x00
+#define VBLANK  0x01
+#define WSYNC   0x02
+#define RSYNC   0x03
+#define NUSIZ0  0x04
+#define NUSIZ1  0x05
+#define COLUP0  0x06
+#define COLUP1  0x07
+#define COLUPF  0x08
+#define COLUBK  0x09
+#define CTRLPF  0x0A
+#define REFP0   0x0B
+#define REFP1   0x0C
+#define PF0     0x0D
+#define PF1     0x0E
+#define PF2     0x0F
+#define RESP0   0x10
+#define RESP1   0x11
+#define RESM0   0x12
+#define RESM1   0x13
+#define RESBL   0x14
+#define AUDC0   0x15
+#define AUDC1   0x16
+#define AUDF0   0x17
+#define AUDF1   0x18
+#define AUDV0   0x19
+#define AUDV1   0x1A
+#define GRP0    0x1B
+#define GRP1    0x1C
+#define ENAM0   0x1D
+#define ENAM1   0x1E
+#define ENABL   0x1F
+#define HMP0    0x20
+#define HMP1    0x21
+#define HMM0    0x22
+#define HMM1    0x23
+#define HMBL    0x24
+#define VDELP0  0x25
+#define VDELP1  0x26
+#define VDELBL  0x27
+#define RESMP0  0x28
+#define RESMP1  0x29
+#define HMOVE   0x2A
+#define HMCLR   0x2B
+#define CXCLR   0x2C
+
+// Read registers
+#define CXM0P   0x00
+#define CXM1P   0x01
+#define CXP0FB  0x02
+#define CXP1FB  0x03
+#define CXM0FB  0x04
+#define CXM1FB  0x05
+#define CXBLPF  0x06
+#define CXPPMM  0x07
+#define INPT0   0x08
+#define INPT1   0x09
+#define INPT2   0x0A
+#define INPT3   0x0B
+#define INPT4   0x0C
+#define INPT5   0x0D
+
+void tia_init(tia_t* c, enum TV tv)
+{
+    c->tv = tv;
+    c->scanline = 0;
+    c->rdy = true;
+}
+
+uint64_t tia_tick(tia_t* c, uint64_t pins)
+{
+    if(c->scanline == 228)
+    {
+        // End of line - time for HSYNC
+        c->scanline = 0;
+        c->rdy = true;
+    }
+        
     if ((pins & (TIA_CS0|TIA_CS1|TIA_CS2|TIA_CS3)) == TIA_CS1) {
         uint8_t addr = TIA_GET_ADDR(pins);
         uint8_t data = TIA_GET_DATA(pins);
+
+        if(TIA_GET_RW(pins))
+        {
+            // Read
+            switch (addr)
+            {
+                case WSYNC:
+                    c->rdy = false;
+                    break;
+
+                case RSYNC:
+                    break;
+
+                case RESP0:
+                    break;
+
+                case RESP1:
+                    break;
+
+                case RESM0:
+                    break;
+
+                case RESM1:
+                    break;
+
+                case RESBL:
+                    break;
+
+                default:
+                    // Write to register
+                    c->regWrite[addr] = data;
+                    break;
+            }
+        }
+        else
+        {
+            // Write
+        }
     }
+
+    // Set RDY latch
+    c->rdy ? TIA_SET_RDY(pins) : TIA_RESET_RDY(pins);
+    // c->scanline == 0 ? TIA_SET_HSYNC(pins) ? TIA_RESET_HSYNC(pins);
+
+    // Keep tack of count on current scanline
+    c->scanline++;
 
     return pins;
 }
