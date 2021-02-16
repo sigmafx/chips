bcef134f5749d3711aeb89bc7506871f796c8ddd
sigmafx <david@sigmafx.co.uk>
2021-02-16 22:31:07 +0000

    Add 6532 and TIA

--- atari/Atari.cpp
+++ atari/Atari.cpp
@@ -7,13 +7,18 @@
 #include "Atari.h"
 #define CHIPS_IMPL
 #include "../chips/m6502.h"
+#include "../chips/m6532.h"
 #include "../chips/cartridge.h"
+#include "../chips/tia.h"
 
 COLORREF screen[228][262];
 HWND hDlgMain;
 int x, y;
+
 cart_t cart;
 m6502_t m6502;
+m6532_t m6532;
+tia_t tia;
 
 void tick(uint8_t red, uint8_t green, uint8_t blue)
 {
@@ -44,14 +49,15 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 {
 	m6502_desc_t m6502_desc;
 	uint64_t pins_6502;
-	uint64_t pins_cart = 0ULL;
-	uint64_t pins_tia = 0ULL;
-	uint64_t pins_6532 = 0ULL;
+	uint64_t pins_cart;
+	uint64_t pins_tia;
+	uint64_t pins_6532;
+	uint8_t clock = 0;
 
     memset(screen, 0x7F, sizeof(DWORD) * 228 * 262);
 	x = y = 0;
 
-	// Initialise the cpu
+	// Initialise CPU
 	m6502_desc.bcd_disabled = true;
 	m6502_desc.m6510_in_cb = NULL;
 	m6502_desc.m6510_out_cb = NULL;
@@ -60,10 +66,17 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	m6502_desc.m6510_io_floating = 0;
 	pins_6502 = m6502_init(&m6502, &m6502_desc);
 
+	// Initialise RIOT
+	pins_6532 = m6532_init(&m6532);
+
+	// Initialsie TIA
+	pins_tia = tia_init(&tia, TV::NTSC);
+
 	// Initialise the cartridge
-	cart_init(&cart);
-	FILE* file = fopen("C:\\Users\\David\\Documents\\GitHub\\chips\\carts\\kernel_01.bin", "rb");
+	pins_cart = cart_init(&cart);
+	FILE* file;
 	uint8_t content[0x1000];
+	fopen_s(&file, "C:\\Users\\David\\Documents\\GitHub\\chips\\carts\\kernel_01.bin", "rb");
 	fread(content, 0x1000, 1, file);
 	fclose(file);
 	cart_load(&cart, content, 0x1000, 0);
@@ -73,85 +86,83 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		uint16_t addr = 0;
 		uint8_t data = 0;
 
-		pins_6502 = m6502_tick(&m6502, pins_6502);
-		addr = M6502_GET_ADDR(pins_6502);
-
-		// Assert address lines of all chips
-		CART_SET_ADDR(pins_cart, addr);
-		//M6532_SET_ADDR(pins_6532, addr);
-		//TIA_SET_ADDR(pins_tia, addr);
-
-		// Set control lines of all chips
-		// 6532
-		//pins_6502 & M6502_A7 ? M6532_SET_CS1(pins_6532) : M6532_RESET_CS1(pins_6532);
-		//pins_6502 & M6502_A12 ? M6532_RESET_CS2(pins_6532) : M6532_SET_CS2(pins_6532);
-		//pins_6502 & M6502_A9 ? M6532_SET_RS(pins_6532) | M6532_RESET_RS(pins_6532);
-		//pins_6502 & M6502_RW ? M6532_SET_RW(pins_6532) | M6532_RESET_RW(pins_6532);
-		// TIA
-		//pins_6502 & M6502_A12 ? TIA_RESET_CS0(pins_tia) : M6532_SET_CS0(pins_tia);
-		//TIA_SET_CS1(pins_tia);
-		//pins_6502 & M6502_A7 ? TIA_RESET_CS1(pins_tia) : TIA_SET_CS1(pins_tia);
-		//pins_6502 & M6502_RW ? TIA_SET_RW(pins_tia) | TIA_RESET_RW(pins_tia);
-
-		if (!(pins_6502 & M6502_RW))
+		if (clock == 0)
 		{
-			// Write
-			// Assert the data lines of all chips
-			CART_SET_DATA(pins_6532, data);
-			//M6532_SET_DATA(pins_6532, data);
-			//TIA_SET_DATA(pins_tia, data);
-		}
+			pins_6502 = m6502_tick(&m6502, pins_6502);
+			addr = M6502_GET_ADDR(pins_6502);
 
-		// Tick the clocks
-		// %3 == 0
-		pins_cart = cart_read(&cart, pins_cart);
-		//pins_6532 = m6532_tick(, pins_6532);
-		// always
-		//pins_tia = tia_tick(, pins_tia);
+			// Assert address lines of all chips
+			CART_SET_ADDR(pins_cart, addr);
+			M6532_SET_ADDR(pins_6532, addr);
+			TIA_SET_ADDR(pins_tia, addr);
 
-		if (pins_6502 & M6502_A12)
-		{
+			// Set control lines of all chips
 			// Cart
-			if (pins_6502 & M6502_RW)
+			pins_6502 & M6502_A12 ? CART_SET_CS(pins_cart) : CART_RESET_CS(pins_cart);
+			// 6532
+			pins_6502 & M6502_A7 ? M6532_SET_CS1(pins_6532) : M6532_RESET_CS1(pins_6532);
+			pins_6502 & M6502_A12 ? M6532_RESET_CS2(pins_6532) : M6532_SET_CS2(pins_6532);
+			pins_6502 & M6502_A9 ? M6532_SET_RS(pins_6532) : M6532_RESET_RS(pins_6532);
+			pins_6502 & M6502_RW ? M6532_SET_RW(pins_6532) : M6532_RESET_RW(pins_6532);
+			// TIA
+			pins_6502 & M6502_A12 ? TIA_RESET_CS0(pins_tia) : TIA_SET_CS0(pins_tia);
+			TIA_SET_CS1(pins_tia);
+			pins_6502 & M6502_A7 ? TIA_RESET_CS1(pins_tia) : TIA_SET_CS1(pins_tia);
+			pins_6502 & M6502_RW ? TIA_SET_RW(pins_tia) : TIA_RESET_RW(pins_tia);
+
+			if (!(pins_6502 & M6502_RW))
 			{
-				// Read
-				data = CART_GET_DATA(pins_cart);
+				// Write
+				// Assert the data lines of all chips
+				CART_SET_DATA(pins_6532, data);
+				M6532_SET_DATA(pins_6532, data);
+				TIA_SET_DATA(pins_tia, data);
 			}
+
+			pins_cart = cart_read(&cart, pins_cart);
+			pins_6532 = m6532_tick(&m6532, pins_6532);
 		}
-		else
-		{
-			if (pins_6502 & M6502_A7)
-			{
-				// 6532
-				if (pins_6502 & M6502_RW)
-				{
-					// Read
-					printf("6532 Read");
-					//data = M6532_GET_DATA(pins_6532);
-				}
-			}
-			else
-			{
-				// TIA
-				if (pins_6502 & M6502_RW)
-				{
-					// Read
-					printf("TIA Read");
-					//data = TIA_GET_DATA(pins_tia);
-				}
-			}
-		}
+
+		pins_tia = tia_tick(&tia, pins_tia);
 
 		if (pins_6502 & M6502_RW)
 		{
+			// Read - determine enabled chip
+			if (pins_6502 & M6502_A12)
+			{
+				// Cart enabled
+				data = CART_GET_DATA(pins_cart);
+			}
+			else
+			{
+				if (pins_6502 & M6502_A7)
+				{
+					// 6532 enabled
+					data = M6532_GET_DATA(pins_6532);
+				}
+				else
+				{
+					// TIA enabled
+					data = TIA_GET_DATA(pins_tia);
+				}
+			}
+
 			// Read
 			// Assert the data lines of all chips
 			M6502_SET_DATA(pins_6502, data);
-			//M6532_SET_DATA(pins_6532, data);
-			//TIA_SET_DATA(pins_tia, data);
+			CART_SET_DATA(pins_cart, data);
+			M6532_SET_DATA(pins_6532, data);
+			TIA_SET_DATA(pins_tia, data);
 		}
 
-		//M6502_SET_RDY(pins_6502, TIA_GET_RDY(pins_tia));
+		// Set RDY
+		TIA_GET_RDY(pins_tia) ? M6502_SET_RDY(pins_6502) : M6502_RESET_RDY(pins_6502);
+
+		clock++;
+		if (clock == 3)
+		{
+			clock = 0;
+		}
 	}
 
     DialogBox(hInstance, MAKEINTRESOURCE(IDD_DLG_MAIN), HWND_DESKTOP, DlgProc);
